bb177962fb88864d6add0c53728c341fb70094f7
ABHIKALVIUM <144119494+ABHIKALVIUM@users.noreply.github.com>
2024-04-27 17:27:33 +0530

    Posted data successfully

--- Backend/Routes/data_routes.js
+++ Backend/Routes/data_routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 var data_routes=express.Router();
 const gameModel=require('../Model/DataModel')
 
+
  data_routes.get("/getdata",async(req,res)=>{
     try{
         const gamedata=await gameModel.find();
@@ -11,6 +12,18 @@ const gameModel=require('../Model/DataModel')
         res.send(err)
     }
 })
+
+data_routes.post("/postdata", async (req, res) => {
+    console.log(req.body);
+    
+    try {
+        const result=await gameModel.insertMany(req.body)
+        res.send("Posted the document successfully")
+    } catch (error) {
+        res.send(error)
+    }
+    
+});
 
 module.exports=data_routes;
 
