c788300b7cb78251601fe57d008a2fdfcf9f1b21
ABHIKALVIUM <144119494+ABHIKALVIUM@users.noreply.github.com>
2024-06-29 14:07:42 +0530

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+
 const connection=require('./config/db');
 const data_routes=require('./Routes/data_routes')
 
@@ -13,12 +14,21 @@ app.use('/data',data_routes)
 
 
 
+
 app.listen(8000, async() => {
+    
     try {
-       await connection;
-       console.log("connected to DB")
+       
+        await connection;
+       
+        console.log("connected to DB")
+    
     } catch (error) {
-       console.log(error)
+       
+        console.log(error)
+    
     }
-     console.log(`🚀 server running on PORT: ${PORT}`);
-  });
+     
+    console.log(`🚀 server running on PORT: ${PORT}`);
+  
+});
