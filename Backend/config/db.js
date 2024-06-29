a9d4b3d07fec3f759314f1e6f8804918595f680e
ABHIKALVIUM <144119494+ABHIKALVIUM@users.noreply.github.com>
2024-06-29 14:28:21 +0530

    Added the read and write branch

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,10 @@
+
 const mongoose=require("mongoose")
+
 require("dotenv").config();
 
+
 let connection=mongoose.connect(process.env.MONGODB_URL);
+
 
 module.exports=connection;
--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+
 const connection=require('./config/db');
 const data_routes=require('./Routes/data_routes')
+const cors=require('cors')
 
-app.use(express.json())
 
-const PORT=process.env.PORT;
+app.use(express.json());
+app.use(cors({
+   origin : "*"
+}))
+
+const PORT=process.env.PORT || 7000;
+console.log(PORT)
+
+app.get("/", (req, res) => {
+   res.send({"msg":"Welcome"})
+})
 
 app.use('/data',data_routes)
 
 
 
 
+
 app.listen(8000, async() => {
-    try {
-       await connection;
-       console.log("connected to DB")
-    } catch (error) {
-       console.log(error)
-    }
-     console.log(`🚀 server running on PORT: ${PORT}`);
-  });
+
+   try {
+
+      await connection;
+
+      console.log("connected to DB")
+
+   } catch (error) {
+
+      console.log(error)
+
+   }
+
+   console.log(`🚀 server running on PORT: ${PORT}`);
+
+});
