ac41a577ac0a27d1a6b936888be8f18f6315c22e
athithyaramaa1 <athithya.ramma@kalvium.community>
2024-07-09 16:46:56 +0530

    feat: Update dependencies and add react-router-dom

--- marketplace-website/src/pages/Main.jsx
+++ marketplace-website/src/pages/Main.jsx
@@ -1,42 +1,64 @@
-import React ,{useState,useEffect} from "react";
-import './Main.css';
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { AppContext } from "../App";
+import "./Main.css";
 import Sidemenu from "../components/Sidemenu";
-import Header from "./Header";
 import Home from "./Home";
-import gta from './GTA.jpg'
+import Header from "./Header";
+import Bag from "./Bag";
+import Categories from "./Categories";
+import MyLibrary from "./MyLibrary";
+import { Route, Routes } from "react-router-dom";
 
-function Main(){
-    const [active,setActive]=useState(false);
-    const [games,setGames]=useState([]);
-    
-    const handleToggleActive=()=>{
-        setActive(!active);
-    }
+function Main() {
+  const { library, bag } = useContext(AppContext);
+  const [active, setActive] = useState(false);
+  const [games, setGames] = useState([]);
 
-    const fetchData=()=>{
-        fetch('http://localhost:8000/data/getdata.json')
-        .then(res=>res.json())
-        .then(data=>{
-            setGames(data)
-        })
-        .catch(e=>console.log(e.message));
-    }
+  const handleToggleActive = () => {
+    setActive(!active);
+  };
 
-    useEffect(()=>{
-        fetchData();
-    },[])
+  const fetchData = () => {
+    fetch("http://localhost:8000/data/getdata")
+      .then((res) => res.json())
+      .then((data) => {
+        setGames(data);
+        console.log(data);
+      })
+      .catch((e) => console.log(e.message));
+  };
 
-    return(
-        <main>
-            <Sidemenu active={active}/>
-            <div className={`banner ${active?'active':undefined}`}>
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-            <Header toggleActive={handleToggleActive}/>
-            <div className="container-fluid">
-                <Home games={games}/>
-            </div>
-            </div>
-        </main>
-    )
+  console.log(games, library, "line 29");
+
+  return (
+    <main className="main">
+      <Sidemenu className="sidemenu" active={active} />
+
+      <div>
+        <Header toggleActive={handleToggleActive} />
+        <div className="container-fluid">
+          {games && games.length > 0 && (
+            <Routes>
+              <Route path="/" element={<Home games={games} />}></Route>
+              <Route
+                path="/category"
+                element={<Categories games={games} />}
+              ></Route>
+              <Route
+                path="/library"
+                element={<MyLibrary games={library} />}
+              ></Route>
+              <Route path="/bag" element={<Bag games={bag} />}></Route>
+            </Routes>
+          )}
+        </div>
+      </div>
+    </main>
+  );
 }
+
 export default Main;
